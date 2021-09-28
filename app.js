66b75d97f9791c4428102c1cc4e6dad9b4c7bfc4
Irmantas <joksas.irmantas@gmail.com>
2021-09-28 22:16:03 +0300

    Post_submit_developement

--- app.js
+++ app.js
@@ -2,34 +2,31 @@ const express = require("express");
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
-const logger = require('./modules/logger');
+const { logEntry, retrieveLog } = require('./modules/logger');
 require('dotenv').config();
 
 const app = express();
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.get("/rates", async (req, res) => {
+app.get("/rates", logEntry, async (req, res) => {
   const api_key = process.env.API_KEY;
   const rates_url = `https://openexchangerates.org/api/latest.json?app_id=${api_key}`;
   const rates_response = await fetch(rates_url);
   const rates_data = await rates_response.json();
-
-  logger.saveLog(req.ip);
 
   const data = {
     time: new Date(),
     ip: req.ip,
     rates: rates_data.rates
   };
-  res.json(data);
+  res.send(data);
 });
 
-app.get('/log/:fromto', async (req, res) => {
-  res.json(logger.retrieveLog(req.params.fromto));
-})
+app.get('/log', retrieveLog);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is listening on port 5000...");
--- modules/logger.js
+++ modules/logger.js
@@ -1,34 +1,7 @@
 const fs = require('fs');
+const _ = require('lodash');
 
-const saveLog = (ip) => {
-
-    let log = [];
-    try {
-        const rawdata = fs.readFileSync('./data/accesslog.json', 'utf-8');
-        log = JSON.parse(rawdata);
-    } catch (err) {
-        console.log(err);
-    }
-    const newDate = new Date().toISOString().slice(0, 10);
-    const logData = {
-        date: newDate,
-        ip: ip
-    }
-    log.push(logData);
-    fs.writeFile('./data/accesslog.json', JSON.stringify(log, null , 2), 'utf-8', (err) => {
-        if (err) {
-          console.log(err);S
-        } else {
-          console.log('The file has been saved!');
-        }
-      });
-};
-
-const retrieveLog = (params) => {
-  const fromTo = params.split(':');
-  const from = new Date(fromTo[0]);
-  const to = new Date(fromTo[1]);
-  let sortedLog = [];
+const logEntry = (req, res, next) => {
   let log = [];
 
   try {
@@ -38,17 +11,54 @@ const retrieveLog = (params) => {
     console.log(err);
   }
 
-  log.map(log_entry => {
-    const logDate = new Date(log_entry.date);
-    if (logDate.getTime() > from.getTime() && logDate.getTime() < to.getTime()) {
-      sortedLog.push(log_entry);
+  const newDate = new Date().toISOString().slice(0, 10);
+  const logData = {
+    date: newDate,
+    ip: req.ip
+  }
+  log.push(logData);
+
+  fs.writeFile('./data/accesslog.json', JSON.stringify(log, null , 2), 'utf-8', (err) => {
+    if (err) {
+      console.log(err);S
+    } else {
+      console.log('The file has been saved!');
     }
   });
+
+  next();
+};
+
+const retrieveLog = (req, res, next) => {
+
+  const from = new Date(req.query.from);
+  const to = new Date(req.query.to);
+  let sortedLog = [];
+  let log = [];
+
+  try {
+    const rawdata = fs.readFileSync('./data/accesslog.json', 'utf-8');
+    log = JSON.parse(rawdata);
+    if(_.isEmpty(req.query)) {
+      res.send(log)
+    } else {
+      log.map(log_entry => {
+        const logDate = new Date(log_entry.date);
+        if (logDate.getTime() > from.getTime() && logDate.getTime() < to.getTime()) {
+          sortedLog.push(log_entry);
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
   if(sortedLog.length != 0 ) {
-    return sortedLog;
+    res.send(sortedLog);
   } else {
-    return 'No log records.'
+    res.send('No log records.');
   }
 }
 
-module.exports = { saveLog, retrieveLog };
+
+module.exports = { logEntry, retrieveLog };
