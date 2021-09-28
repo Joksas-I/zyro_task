708182b3de05e0c86dc99597e0f411151b42aac3
Irmantas <joksas.irmantas@gmail.com>
2021-09-28 22:22:34 +0300

    Revert "Post_submit_developement"
    This reverts commit 66b75d97f9791c4428102c1cc4e6dad9b4c7bfc4.

--- modules/logger.js
+++ modules/logger.js
@@ -1,64 +1,54 @@
 const fs = require('fs');
-const _ = require('lodash');
 
-const logEntry = (req, res, next) => {
-  let log = [];
+const saveLog = (ip) => {
 
-  try {
-    const rawdata = fs.readFileSync('./data/accesslog.json', 'utf-8');
-    log = JSON.parse(rawdata);
-  } catch (err) {
-    console.log(err);
-  }
-
-  const newDate = new Date().toISOString().slice(0, 10);
-  const logData = {
-    date: newDate,
-    ip: req.ip
-  }
-  log.push(logData);
-
-  fs.writeFile('./data/accesslog.json', JSON.stringify(log, null , 2), 'utf-8', (err) => {
-    if (err) {
-      console.log(err);S
-    } else {
-      console.log('The file has been saved!');
+    let log = [];
+    try {
+        const rawdata = fs.readFileSync('./data/accesslog.json', 'utf-8');
+        log = JSON.parse(rawdata);
+    } catch (err) {
+        console.log(err);
     }
-  });
-
-  next();
+    const newDate = new Date().toISOString().slice(0, 10);
+    const logData = {
+        date: newDate,
+        ip: ip
+    }
+    log.push(logData);
+    fs.writeFile('./data/accesslog.json', JSON.stringify(log, null , 2), 'utf-8', (err) => {
+        if (err) {
+          console.log(err);S
+        } else {
+          console.log('The file has been saved!');
+        }
+      });
 };
 
-const retrieveLog = (req, res, next) => {
-
-  const from = new Date(req.query.from);
-  const to = new Date(req.query.to);
+const retrieveLog = (params) => {
+  const fromTo = params.split(':');
+  const from = new Date(fromTo[0]);
+  const to = new Date(fromTo[1]);
   let sortedLog = [];
   let log = [];
 
   try {
     const rawdata = fs.readFileSync('./data/accesslog.json', 'utf-8');
     log = JSON.parse(rawdata);
-    if(_.isEmpty(req.query)) {
-      res.send(log)
-    } else {
-      log.map(log_entry => {
-        const logDate = new Date(log_entry.date);
-        if (logDate.getTime() > from.getTime() && logDate.getTime() < to.getTime()) {
-          sortedLog.push(log_entry);
-        }
-      });
-    }
   } catch (err) {
     console.log(err);
   }
 
+  log.map(log_entry => {
+    const logDate = new Date(log_entry.date);
+    if (logDate.getTime() > from.getTime() && logDate.getTime() < to.getTime()) {
+      sortedLog.push(log_entry);
+    }
+  });
   if(sortedLog.length != 0 ) {
-    res.send(sortedLog);
+    return sortedLog;
   } else {
-    res.send('No log records.');
+    return 'No log records.'
   }
 }
 
-
-module.exports = { logEntry, retrieveLog };
+module.exports = { saveLog, retrieveLog };
